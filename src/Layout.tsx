f6b750b861a49432e6b4e50699316e308dc4cabc
luckystar628 <luckystar000628@gmail.com>
2024-06-21 14:22:37 +0900

    add telegram user

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,25 @@
 import { Outlet, useLocation } from "react-router-dom";
+import { useGlobalContext } from "./context/GlobalContext";
 import Footer from "./component/Footer";
+import { useEffect } from "react";
 export default function Layout() {
   const location = useLocation();
+  const { setUser, user } = useGlobalContext();
+  useEffect(() => {
+    const webapp = (window as any).Telegram?.WebApp.initDataUnsafe;
+    if (webapp && webapp["user"]) {
+      setUser({
+        username: webapp["user"]["username"],
+        id: webapp["user"]["id"],
+        score: 0,
+        isInvited: false,
+      })
+    } 
+  }, [])
+
+  useEffect(() => {
+    console.log("=========>user", user);
+  }, [user])
 
   return (
     <div className="relative w-full h-screen flex flex-col justify-between bg-gradient-bottom-center p-5 poppins-thin">
