621e4f32182142911a9c9135b5cffb8e1e4e8fe4
luckystar628 <luckystar000628@gmail.com>
2024-06-27 03:39:16 +0900

    update get function

--- src/page/DailyReport.tsx
+++ src/page/DailyReport.tsx
@@ -9,9 +9,10 @@ export default function DailyReport() {
   const [selectedOption, setSelectedOption] = useState<number>(0);
   const [isCompleted, setIsCompleted] = useState<boolean>(false);
   useEffect(() => {
-    axios.get(`${ backend }/question/get`).then((res) => {
+    axios.get(`${backend}/question/get`).then((res) => {
+      console.log(res.data);
       setTodaysData(res.data);
-    })
+    });
   }, []);
   return (
     <div className="px-8 py-2 max-sm:px-0 grow">
@@ -34,24 +35,30 @@ export default function DailyReport() {
       ) : (
         <>
           {" "}
-          <div className="text-2xl max-sm:text-[16px]">{todaysData.question || "Loading..."}</div>
+          <div className="text-2xl max-sm:text-[16px]">
+            {todaysData.question || "Loading..."}
+          </div>
           <div className="gap-5 mt-5 flex flex-col overflow scrollbar-hidden">
-            {todaysData.question.options &&
-              todaysData.question.options.map((option: string, index: number) => {
-                return (
-                  <div
-                    key={index}
-                    className={`max-sm:text-[14px] transition shadow-lg ease-in-out cursor-pointer py-5 max-sm:py-3 rounded-xl border-2 hover:bg-opacity-60 active:bg-opacity-50 ${
-                      selectedOption === index
-                        ? "bg-[#1A1A1A] bg-opacity-100 border-[#1A1A1A]"
-                        : "bg-[#625f63] bg-opacity-30 border-[#ffffff33]"
-                    }`}
-                    onClick={() => setSelectedOption(index)}
-                  >
-                    {option}
-                  </div>
-                );
-              })}
+            {todaysData &&
+              todaysData.question &&
+              todaysData.question.options &&
+              todaysData.question.options.map(
+                (option: string, index: number) => {
+                  return (
+                    <div
+                      key={index}
+                      className={`max-sm:text-[14px] transition shadow-lg ease-in-out cursor-pointer py-5 max-sm:py-3 rounded-xl border-2 hover:bg-opacity-60 active:bg-opacity-50 ${
+                        selectedOption === index
+                          ? "bg-[#1A1A1A] bg-opacity-100 border-[#1A1A1A]"
+                          : "bg-[#625f63] bg-opacity-30 border-[#ffffff33]"
+                      }`}
+                      onClick={() => setSelectedOption(index)}
+                    >
+                      {option}
+                    </div>
+                  );
+                }
+              )}
           </div>
           <div
             className="mt-10 max-sm:mt-5 max-sm:py-3 text-center bg-white py-5 rounded-md hover:opacity-80 active:opacity-50 text-black cursor-pointer"
@@ -61,7 +68,6 @@ export default function DailyReport() {
           </div>
         </>
       )}
-      
     </div>
   );
 }
