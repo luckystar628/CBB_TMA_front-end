8cece41e2e785dede0166a2fcc01332db989d1fc
luckystar628 <luckystar000628@gmail.com>
2024-06-29 07:25:42 +0900

    update airdrop page

--- src/page/Airdrop.tsx
+++ src/page/Airdrop.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 import { useGlobalContext } from "../context/GlobalContext";
+import { Link } from "react-router-dom";
 
 export default function Airdrop() {
   const { user } = useGlobalContext();
@@ -37,31 +38,36 @@ export default function Airdrop() {
     <div className="mx-10 max-sm:mx-0">
       <div className="text-2xl font-[600] max-sm:text-lg">complete daily actions<br /> for your chance to win blob</div>
       <div className="flex flex-col gap-6 max-sm:gap-4 my-10 max-sm:my-5 overflow-auto">
-        <div className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[0] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+        <Link to="https://twitter.com/nomadworld_ai" className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[0] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[0] && handleLink(0)}
         >
-          follow us on X (+1 coin)
-        </div>
-        <div className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[1] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+          follow us on X
+          <div className="flex"><img src="/image/coin.svg" /> +1</div>
+        </Link>
+        <Link to="https://twitter.com/nomadworld_ai" className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[1] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[1] && handleLink(1)}
         >
-          retweet, like, and reply to this tweet (+1 coin)
-        </div>
-        <div className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[2] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+          retweet, like, and reply to this tweet
+          <div className="flex"><img src="/image/coin.svg" /> +1</div>
+        </Link>
+        <Link to="https://nomadworld.ai" className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[2] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[2] && handleLink(2)}
         >
-          visit our website (+1 coin)
-        </div>
-        <div className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[3] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+          visit our website
+          <div className="flex"><img src="/image/coin.svg" /> +1</div>
+        </Link>
+        <Link to="https://www.tiktok.com/@nomadworld.ai" className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[3] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[3] && handleLink(3)}
         >
-          follow us on tiktok (+1 coin)
-        </div>
-        <div className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[4] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+          follow us on tiktok
+          <div className="flex"><img src="/image/coin.svg" /> +1</div>
+        </Link>
+        <Link to="https://www.instagram.com/nomadworld.ai" className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[4] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[4] && handleLink(4)}
         >
-          follow us on instagram (+1 coin)
-        </div>
+          follow us on instagram
+          <div className="flex"><img src="/image/coin.svg" /> +1</div>
+        </Link>
       </div>
     </div>
   );
