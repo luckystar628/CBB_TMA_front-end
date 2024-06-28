cecd1ce203bd763f8e0b009796da20332564d312
luckystar628 <luckystar000628@gmail.com>
2024-06-29 05:07:03 +0900

    add log for isCompleted

--- src/page/Airdrop.tsx
+++ src/page/Airdrop.tsx
@@ -1,21 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Airdrop() {
+  const [isCompleted, setIsCompleted] = useState<any>([false, false, false, false, false]);
+  const backend = import.meta.env.VITE_BACKEND_URL;
+  useEffect(() => {
+    console.log("isCompleted: ", isCompleted);
+  }, [isCompleted])
+  const handleLink = (index: number) => {
+    axios.post(`${backend}/task/add/${index}`).then(res => {
+      console.log(res);
+      setIsCompleted((pre: any) => { const newState = [...pre]; newState[index] = true; return newState; })
+    })
+  }
   return (
     <div className="mx-10 max-sm:mx-0">
       <div className="text-2xl font-[600] max-sm:text-lg">complete daily actions<br /> for your chance to win blob</div>
       <div className="flex flex-col gap-6 max-sm:gap-4 my-10 max-sm:my-5">
-        <div className="text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2">
+        <div className={`text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2 ${isCompleted[0] && "bg-[#212342]"}`}
+          onClick={() => handleLink(0)}
+        >
           follow us on X
         </div>
-        <div className="text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2">
+        <div className={`text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2 ${isCompleted[1] && "bg-[#212342]"}`}
+          onClick={() => handleLink(1)}
+        >
           retweet, like, and reply to this tweet
         </div>
-        <div className="text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2">
+        <div className={`text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2 ${isCompleted[2] && "bg-[#212342]"}`}
+          onClick={() => handleLink(2)}
+        >
           visit our website
         </div>
-        <div className="text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2">
+        <div className={`text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2 ${isCompleted[3] && "bg-[#212342]"}`}
+          onClick={() => handleLink(3)}
+        >
           follow us on tiktok
         </div>
-        <div className="text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2">
+        <div className={`text-lg text-[16px] font-light py-3 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10 rounded-xl bg-[#ffffff] bg-opacity-10 shadow-lg border-[#ffffff33] border-2 ${isCompleted[4] && "bg-[#212342]"}`}
+          onClick={() => handleLink(4)}
+        >
           follow us on instagram
         </div>
       </div>
