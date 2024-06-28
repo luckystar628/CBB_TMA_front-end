f6eabca3d5214a2f8bc37ad2300318654409cd46
luckystar628 <luckystar000628@gmail.com>
2024-06-28 22:05:35 +0900

    update share url

--- src/page/Friends.tsx
+++ src/page/Friends.tsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
 import { rankingList } from "../assets/const";
 import ShareReferral from "../component/ShareReferral";
-import { initUtils } from '@tma.js/sdk';
+// import { initUtils } from "@tma.js/sdk";
+
+import { useGlobalContext } from "../context/GlobalContext";
 
 export default function Friends() {
-  const utils = initUtils();
+  const { user } = useGlobalContext();
+  // const utils = initUtils();
   // const openTelegram = () => {
   //   utils.openTelegramLink(
   //     `https://t.me/share/url?url=https://t.me/catizenbot/gameapp?startapp=rp_1365932&text=%F0%9F%92%B0Catizen%3A%20Unleash%2C%20Play%2C%20Earn%20-%20Where%20Every%20Game%20Leads%20to%20an%20Airdrop%20Adventure!%0A%F0%9F%8E%81Let%27s%20play-to-earn%20airdrop%20right%20now!`
@@ -12,20 +14,16 @@ export default function Friends() {
   // }
 
   const shareTelegram = () => {
-    utils.shareURL('https://t.me/mybot/myapp', 'Look! Some cool app here!');
-  }
+    const url = "https://t.me/mybot/myapp";
+    const text = "Look! Some cool app here!"
+    window.open(`tg://msg_url?url=${url}&text=${text}`);
+    // utils.shareURL("https://t.me/mybot/myapp", "Look! Some cool app here!");
+  };
 
   // const [tab, setTab] = useState(0);
-  const [user, setUser] = useState<any>({});
   // const handleTab = (index: number) => {
   //   setTab(index);
   // };
-  useEffect(() => {
-    const newUser = rankingList.find(
-      (item) => item.username === "@currentuser"
-    );
-    setUser(newUser);
-  }, []);
   return (
     <>
       <div className="mx-10 max-sm:mx-0 mt-10 max-sm:mt-2 py-3 max-sm:py-2 rounded-md bg-[#232124] bg-opacity-30 border-[#ffffff33] border-2">
@@ -67,36 +65,37 @@ export default function Friends() {
       </ul> */}
       <div className="mx-10 max-sm:mx-0 overflow-auto scrollbar-hidden my-2">
         {rankingList.map((item, index) => (
-              <div className="flex items-center">
-                <div
-                  className={`flex w-full justify-between items-center mx-5 p-3 ${
-                    item.username === user.username &&
-                    "rounded-2xl bg-[#232124] bg-opacity-30 border-[#ffffff33] border-2"
-                  }`}
-                >
-                  <div>
-                    <span className="mr-3">{index + 1}.</span>
-                    <span>{item.username}</span>
-                  </div>
-                  <span>
-                    {item.score ? `${item.score} coins` : "share to earn"}
-                  </span>
-                </div>
-                <img
-                  src={
-                    item.isInvited
-                      ? "/image/icon-checked.svg"
-                      : "/image/icon-telegram.svg"
-                  }
-                  alt="avatar"
-                  className={`mx-5 max-sm:ml-0 w-6 h-6 ${
-                    !item.isInvited && "cursor-pointer"
-                  }`}
-                  onClick={() => { !item.isInvited && shareTelegram(); }}
-                />
+          <div className="flex items-center">
+            <div
+              className={`flex w-full justify-between items-center mx-5 p-3 ${
+                item.username === user.username &&
+                "rounded-2xl bg-[#232124] bg-opacity-30 border-[#ffffff33] border-2"
+              }`}
+            >
+              <div>
+                <span className="mr-3">{index + 1}.</span>
+                <span>{item.username}</span>
               </div>
-            ))
-          }
+              <span>
+                {item.score ? `${item.score} coins` : "share to earn"}
+              </span>
+            </div>
+            <img
+              src={
+                item.isInvited
+                  ? "/image/icon-checked.svg"
+                  : "/image/icon-telegram.svg"
+              }
+              alt="avatar"
+              className={`mx-5 max-sm:ml-0 w-6 h-6 ${
+                !item.isInvited && "cursor-pointer"
+              }`}
+              onClick={() => {
+                !item.isInvited && shareTelegram();
+              }}
+            />
+          </div>
+        ))}
       </div>
       <div className="mb-10 mx-10 max-sm:mx-0">
         <ShareReferral />
