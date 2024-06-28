31e90a63d751ce94c93f19e427fc14bd57523ac2
luckystar628 <luckystar000628@gmail.com>
2024-06-29 07:45:33 +0900

    update size

--- src/page/Airdrop.tsx
+++ src/page/Airdrop.tsx
@@ -38,35 +38,35 @@ export default function Airdrop() {
     <div className="mx-10 max-sm:mx-0">
       <div className="text-2xl font-[600] max-sm:text-lg">complete daily actions<br /> for your chance to win blob</div>
       <div className="flex flex-col gap-6 max-sm:gap-4 my-10 max-sm:my-5 overflow-auto">
-        <Link to="https://twitter.com/nomadworld_ai" className={`text-lg text-[16px] flex justify-center items-center text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[0] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+        <Link to="https://twitter.com/nomadworld_ai" className={`text-lg text-[16px] flex justify-center items-center gap-3 text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[0] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[0] && handleLink(0)}
         >
           follow us on X
-          <div className="flex justify-center items-center"><img src="/image/Coin_small.png" className="w-8 h-8" /> +1</div>
+          <div className="flex justify-center items-center gap-2"><img src="/image/Coin_small.png" className="w-4 h-4" /> +1</div>
         </Link>
-        <Link to="https://twitter.com/nomadworld_ai" className={`text-lg text-[16px] flex justify-center items-center text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[1] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+        <Link to="https://twitter.com/nomadworld_ai" className={`text-lg text-[16px] flex justify-center items-center gap-3 text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[1] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[1] && handleLink(1)}
         >
           retweet, like, and reply to this tweet
-          <div className="flex justify-center items-center"><img src="/image/Coin_small.png" className="w-8 h-8" /> +1</div>
+          <div className="flex justify-center items-center gap-2"><img src="/image/Coin_small.png" className="w-4 h-4" /> +1</div>
         </Link>
-        <Link to="https://nomadworld.ai" className={`text-lg text-[16px] flex justify-center items-center text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[2] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+        <Link to="https://nomadworld.ai" className={`text-lg text-[16px] flex justify-center items-center gap-3 text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[2] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[2] && handleLink(2)}
         >
           visit our website
-          <div className="flex justify-center items-center"><img src="/image/Coin_small.png" className="w-8 h-8" /> +1</div>
+          <div className="flex justify-center items-center gap-2"><img src="/image/Coin_small.png" className="w-4 h-4" /> +1</div>
         </Link>
-        <Link to="https://www.tiktok.com/@nomadworld.ai" className={`text-lg text-[16px] flex justify-center items-center text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[3] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+        <Link to="https://www.tiktok.com/@nomadworld.ai" className={`text-lg text-[16px] flex justify-center items-center gap-3 text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[3] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[3] && handleLink(3)}
         >
           follow us on tiktok
-          <div className="flex justify-center items-center"><img src="/image/coin.svg" /> +1</div>
+          <div className="flex justify-center items-center gap-2"><img src="/image/Coin_small.png" /> +1</div>
         </Link>
-        <Link to="https://www.instagram.com/nomadworld.ai" className={`text-lg text-[16px] flex justify-center items-center text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[4] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
+        <Link to="https://www.instagram.com/nomadworld.ai" className={`text-lg text-[16px] flex justify-center items-center gap-3 text-white font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[4] ? "bg-[#2F9A21] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[4] && handleLink(4)}
         >
           follow us on instagram
-          <div className="flex justify-center items-center"><img src="/image/coin.svg" /> +1</div>
+          <div className="flex justify-center items-center gap-2"><img src="/image/Coin_small.png" /> +1</div>
         </Link>
       </div>
     </div>
