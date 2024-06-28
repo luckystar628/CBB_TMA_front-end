d4b5070fbfff505a3a93e1d1e83594f3a35fb185
luckystar628 <luckystar000628@gmail.com>
2024-06-29 05:48:01 +0900

    update overflow

--- src/page/Airdrop.tsx
+++ src/page/Airdrop.tsx
@@ -36,7 +36,7 @@ export default function Airdrop() {
   return (
     <div className="mx-10 max-sm:mx-0">
       <div className="text-2xl font-[600] max-sm:text-lg">complete daily actions<br /> for your chance to win blob</div>
-      <div className="flex flex-col gap-6 max-sm:gap-4 my-10 max-sm:my-5">
+      <div className="flex flex-col gap-6 max-sm:gap-4 my-10 max-sm:my-5 overflow-auto">
         <div className={`text-lg text-[16px] font-light py-3 rounded-xl shadow-lg border-[#ffffff33] border-2 ${isCompleted[0] ? "bg-[#55CF45] cursor-not-allowed" : "bg-[#ffffff] bg-opacity-10 cursor-pointer hover:bg-opacity-20 active:bg-opacity-10"}`}
           onClick={() => !isCompleted[0] && handleLink(0)}
         >
