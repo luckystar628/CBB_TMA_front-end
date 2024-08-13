c73759255d9835c257513ca8a8ceb0e9458c987c
luckystar628 <luckystar000628@gmail.com>
2024-08-14 02:30:16 +1000

    fsdfsdfs

--- src/page/DailyReport.tsx
+++ src/page/DailyReport.tsx
@@ -24,7 +24,7 @@ export default function DailyReport() {
           setIsCompleted(true);
           console.log(err);
         });
-      }, 10000);
+      }, 3000);
   });
 
   const handleSubmit = async () => {
