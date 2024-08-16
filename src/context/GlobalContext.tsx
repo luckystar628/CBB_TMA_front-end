ed2ecedfed199fdc18be3ecddf879e05414e5734
luckystar628 <luckystar000628@gmail.com>
2024-08-16 22:13:11 +1000

    set questions

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useContext, useState } from "react";
 interface GlobalContextType {
     user: { username: string, score: number, id: string, isInvited: boolean };
     setUser: React.Dispatch<React.SetStateAction<{ username: string, score: number, id: string, isInvited: boolean }>>;
+    questionCounter: number;
+    setQuestionCounter: React.Dispatch<React.SetStateAction<number>>;  // for updating the question counter state.  This is used for tracking which question the user is on. 0-indexed.  If the user answers all questions, the counter will be set to 1000.  This is a temporary solution until we have a better way to track questions.  A more complex solution might involve using Redux or a similar state management library.  But for the sake
 }
 
 interface GlobalProvideProps {
@@ -14,9 +16,10 @@ const GlobalContext = createContext<GlobalContextType | null>(null);
 export const GlobalProvider: React.FC<GlobalProvideProps> = ({ children }) => {
 
     const [user, setUser] = useState({ username: "", score: 0, id: "", isInvited: false });
+    const [questionCounter, setQuestionCounter] = useState<number>(0); 
 
     return (
-        <GlobalContext.Provider value={{ user, setUser }}>
+        <GlobalContext.Provider value={{ user, questionCounter, setUser, setQuestionCounter }}>
             {children}
         </GlobalContext.Provider>
     );
